5642cb106d05f0a7cc9d4f900e8a8025437a8073
Anastasia <a.khotenovskaia@gmail.com>
2023-03-09 13:31:37 +0300

    Some fix

--- src/components/homeElements/OneRecipe.jsx
+++ src/components/homeElements/OneRecipe.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import axios from 'axios';
+
 // Это компонент, который оображается в Home page
 
 export default function OneRecipe({ recipe }) {
+  const [favourite, setFavourite] = useState('');
+  const favHandler = () => {
+    await axios(`/favourite/${id}`, { method: 'put' });
+    setFavourite((prev) => prev.filter((el) => el.id !== id));
+  };
   return (
     <div className='card one-recipe' style={{ width: '18rem' }}>
       <a href='#'>
@@ -14,7 +21,7 @@ export default function OneRecipe({ recipe }) {
           <li className='list-group-item'>Cooking time: {recipe.time}</li>
         </ul>
         <div className='heart'>
-          <img src='img/heart.png' alt='' />
+          <img onClick={favHandler} src='img/heart.png' alt='' />
         </div>
       </a>
       <hr />
