8d23b4f3e9d70af31c2d70b3833fe5d07766cfe2
pavel <pasha_nikolaev@bk.ru>
2023-03-09 16:45:20 +0300

    randomFoodCategory

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,15 @@ export default function Home() {
   const [recipes1, setRecipes] = useState([]);
   // Создать переменную с массивом категор(ий и после буквы q запихать
   // Возможно логику запроса из юзеффекта надо вынести в функцию
+  const category = () => {
+    const hardCodeCategoryFood = ['soup', 'salad', 'burger', 'paste', 'meat', 'drink', 'cake', 'bread', 'cereals', 'desserts', 'preserve', 'preps', 'sandwiches', 'starter'];
+    const random = Math.floor(Math.random() * hardCodeCategoryFood.length);
+    return hardCodeCategoryFood[random];
+  };
+
   useEffect(() => {
     fetch(
-      'https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=soup&app_id=ea8b9ed4&app_key=%20dd76fee58274a583ce5ee3e916c01595%09&random=true',
+      `https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=${category()}&app_id=ea8b9ed4&app_key=%20dd76fee58274a583ce5ee3e916c01595%09&random=true`,
     )
       .then((res) => res.json())
 
