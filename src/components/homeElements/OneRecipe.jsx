a4d655ff1ddfbf1385389c8e619326e3bade0706
Yurkovskiy Aleksandr <alus03@mail.ru>
2023-03-09 17:25:24 +0300

    propagation fix

--- src/components/homeElements/OneRecipe.jsx
+++ src/components/homeElements/OneRecipe.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import ReceipePage from './ReceipePage';
+import React, { useState } from "react";
+import axios from "axios";
+import ReceipePage from "./ReceipePage";
 
 // Это компонент, который оображается в Home page
 
 export default function OneRecipe({ recipe, setOneRecipePage, user }) {
-  const favHandler = async () => {
-    await axios.put('/favourite', recipe);
+  const favHandler = async (e) => {
+    e.stopPropagation();
+    await axios.put("/favourite", recipe);
   };
   return (
     <div
@@ -14,21 +15,21 @@ export default function OneRecipe({ recipe, setOneRecipePage, user }) {
         console.log(recipe);
         setOneRecipePage(recipe);
       }}
-      className='card one-recipe'
-      style={{ width: '18rem' }}
+      className="card one-recipe"
+      style={{ width: "18rem" }}
     >
-      <img src={recipe.img} className='card-img-top' alt='' />
-      <div className='card-body'>
-        <h5 className='card-title'>{recipe.name}</h5>
+      <img src={recipe.img} className="card-img-top" alt="" />
+      <div className="card-body">
+        <h5 className="card-title">{recipe.name}</h5>
       </div>
-      <ul className='list-group list-group-flush '>
-        <li className='list-group-item'>Ingredients: {recipe.ingredients}</li>
-        <li className='list-group-item'>Cooking time: {recipe.time}</li>
+      <ul className="list-group list-group-flush ">
+        <li className="list-group-item">Ingredients: {recipe.ingredients}</li>
+        <li className="list-group-item">Cooking time: {recipe.time}</li>
       </ul>
       <div>
         {user ? (
-          <div className='heart'>
-            <img onClick={favHandler} src='img/heart.png' alt='' />
+          <div className="heart">
+            <img onClick={favHandler} src="img/heart.png" alt="" />
           </div>
         ) : (
           <></>
