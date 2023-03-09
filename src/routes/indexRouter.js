bf81d79d35d357901dbb3d66c40832dea8fbd792
Yurkovskiy Aleksandr <alus03@mail.ru>
2023-03-09 13:01:00 +0300

    recPage

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,10 +1,15 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const initState = {};
-  res.render('Layout', initState);
+  res.render("Layout", initState);
+});
+
+router.get("/test", (req, res) => {
+  const initState = {};
+  res.render("Layout", initState);
 });
 
 export default router;
